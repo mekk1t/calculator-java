4cd404cf4908dd09bfadc007b1e057506a28f693
mekk1t <kit.kalimov@gmail.com>
2022-05-12 19:04:29 +0300

    Created project

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+
+    public int Sum(int first, int second) {
+        return first + second;
+    }
+
+    public int Subtract(int from, int value) {
+        return from - value;
+    }
+}
