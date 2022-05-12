4b191f94b4b0fedb72804d651f13c27700d9111a
mekk1t <kit.kalimov@gmail.com>
2022-05-12 19:07:13 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println("Hello, calculator!");
+        System.out.println(calculator.multiply(20, 12));
     }
 }
